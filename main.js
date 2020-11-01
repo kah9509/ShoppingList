081a42a8f3052489450ae121a82bc64937eb68c3
Kim Ahyn <kah9509@gmail.com>
2020-11-01 23:07:26 +0900

    이벤트 위임으로 li에 매번 생성되던 이벤트를 부모에만 둠

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
-const addBtn = document.querySelector('.footer__button');
+
+// 부모 (이벤트위임으로 바꿈)
+const list = document.querySelector('.list');
+list.addEventListener('click', (event) => {
+  if (event.target.className === 'fas fa-plus' || event.target.className === 'footer__button') {
+    onAdd();
+  }
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
+  }
+});
+list.addEventListener('keypress', (event) => {
+  if (event.target.className === 'footer__input' && event.key === 'Enter') {
+    onAdd();
+  }
+});
 
 function onAdd(params) {
   // 1. 사용자가 입력한 텍스트를 받아와야함
@@ -11,7 +28,6 @@ function onAdd(params) {
   }
   // 2. 새로운 아이템을 만들어야함. (텍스트 + 삭제 버튼)
   const item = createItem(text);
-  console.log('item', item);
   // 3. items 에 추가한다
   items.appendChild(item);
   //4. 새로 추가된 아이템으로 스크롤링
@@ -21,42 +37,19 @@ function onAdd(params) {
   input.focus();
 }
 
+let id = 0;
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+    <div class="item">
+      <span class="item__name">${text}</span>
+      <button class="item__delete" >
+        <i class="fas fa-trash-alt" data-id=${id}></i>
+      </button>
+    </div>
+    <div class="item__divider"></div>`;
+    id++;
   return itemRow;
 }
-
-addBtn.addEventListener('click', () => {
-  onAdd();
-})
-
-input.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    onAdd();
-  }
-  console.log('key!!');
-})
